686bfd727767800aca0abaa68213f060161b1947
Johan Charpentier <cyberj@arcagenis.org>
2011-04-23 13:27:33 +0200

    Other version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 from setuptools import setup, find_packages
 
-version = '0.1.1'
+version = '0.1.2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
